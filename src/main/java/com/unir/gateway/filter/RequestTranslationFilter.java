b64bfc73903d4d5be604a1d5f0377408b97b146a
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2024-01-05 12:33:52 +0100

    Return 400 if request is not valid (doc)

--- src/main/java/com/unir/gateway/filter/RequestTranslationFilter.java
+++ src/main/java/com/unir/gateway/filter/RequestTranslationFilter.java
@@ -19,8 +19,7 @@ import reactor.core.publisher.Mono;
  * This class is a custom filter for the Spring Cloud Gateway. It is responsible for translating incoming requests.
  * It uses the RequestBodyExtractor to extract the body of the request and the RequestDecoratorFactory to create a decorator for the request.
  * The decorator is used to modify the request before it is forwarded to the downstream service.
- * If the incoming request does not have a content type, it is forwarded without any modifications.
- * Otherwise, the body of the request is joined into a single DataBuffer, and the request is mutated using the decorator before being forwarded.
+ * By default, the response status is set to 400 (Bad Request). This will be overridden if the request is valid.
  */
 @Component
 @RequiredArgsConstructor
@@ -32,9 +31,10 @@ public class RequestTranslationFilter implements GlobalFilter {
 
     /**
      * This method is the main filter method for the Spring Cloud Gateway.
-     * It checks if the incoming request has a content type. If it does not, the request is forwarded without any modifications.
-     * If the request does have a content type, the body of the request is joined into a single DataBuffer.
+     * It checks if the incoming request has a content type and is a POST request.
+     * If the request does have a content type and is a POST request, the body of the request is joined into a single DataBuffer.
      * Then, the request is mutated using the decorator before being forwarded.
+     * By default, the response status is set to 400 (Bad Request). This will be overridden if the request is valid.
      *
      * @param exchange the current server web exchange
      * @param chain the gateway filter chain
