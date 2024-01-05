5b244c676099153559aebfdb712be535b5ba041f
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2024-01-05 12:31:28 +0100

    Return 400 if request is not valid

--- src/main/java/com/unir/gateway/filter/RequestTranslationFilter.java
+++ src/main/java/com/unir/gateway/filter/RequestTranslationFilter.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -43,8 +45,13 @@ public class RequestTranslationFilter implements GlobalFilter {
             ServerWebExchange exchange,
             GatewayFilterChain chain) {
 
-        if (exchange.getRequest().getHeaders().getContentType() == null) {
-            return chain.filter(exchange);
+        // By default, set the response status to 400. This will be overridden if the request is valid.
+        exchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST);
+
+        // Simple check to see if the request has a content type and is a POST request
+        if (exchange.getRequest().getHeaders().getContentType() == null || !exchange.getRequest().getMethod().equals(HttpMethod.POST)) {
+            log.info("Request does not have a content type or is not a POST request");
+            return exchange.getResponse().setComplete();
         } else {
             return DataBufferUtils.join(exchange.getRequest().getBody())
                     .flatMap(dataBuffer -> {
